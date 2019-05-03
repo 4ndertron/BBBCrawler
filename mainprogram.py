4afcac40565f21aaeb988d5135b9e9708099a5d5
4ndertron <robert.anderson@vivintsolar.com>
2019-05-03 13:41:39 -0600

    Created project, and testing venv integration into git. Updated project objective and todo's.

---
+++ mainprogram.py
@@ -0,0 +1,7 @@
+"""
+The objective of this program is to transfer all, or most, of the BBB portal information into the data warehouse.
+TODO: Test login and line item export with Selenium and with headless.
+TODO: Test snowflake connection
+TODO: Test snowflake upsert based on line item criteria.
+"""
+
